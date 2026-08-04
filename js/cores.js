5b97b6e39d1c1316f50060470e434aaa5befb28e
ggblockster <ggblockster@proton.me>
2026-08-04 20:41:05 +1000

    Update cores.js

--- js/cores.js
+++ js/cores.js
@@ -132,3 +132,104 @@ export function getCore(filename) {
             .toLowerCase();
     return cores[ext] ?? null;
 }
+
+export function getPlatform(filename) {
+    const ext =
+        filename
+            .split(".")
+            .pop()
+            .toLowerCase();
+
+    switch(ext) {
+        case "gb":
+            return "gb";
+
+        case "gbc":
+            return "gbc";
+
+        case "gba":
+            return "gba";
+
+        case "n64":
+        case "z64":
+        case "v64":
+            return "n64";
+
+        case "nds":
+            return "nds";
+
+        case "nes":
+        case "fds":
+        case "unf":
+        case "unif":
+            return "nes";
+
+        case "gg":
+            return "gg";
+
+        case "gen":
+        case "md":
+        case "smd":
+            return "segaMd";
+
+        case "sms":
+            return "sms";
+
+        case "sfc":
+        case "smc":
+            return "snes";
+
+        case "3ds":
+        case "3dsx":
+        case "cia":
+        case "cci":
+        case "cxi":
+            return "3ds";
+
+        default:
+            return null;
+    }
+}
+
+export function getConsoleSVG(platform) {
+    if (!platform) return;
+    switch (platform) {
+        case "gb":
+        case "gbc":
+            return "/assets/img/controllers/gameboy.svg";
+        case "gba":
+            return "/assets/img/controllers/gba.svg";
+        case "n64":
+            return "/assets/img/controllers/n64.svg";
+        case "nes":
+            return "/assets/img/controllers/nes.svg";
+        case "nds":
+            return "/assets/img/controllers/nds.svg";
+        case "gg":
+            return "/assets/img/controllers/game gear.svg";
+        case "segaMd":
+            return "/assets/img/controllers/megadrive.svg";
+        case "sms":
+            return "/assets/img/controllers/master system.svg";
+        case "snes":
+            return "/assets/img/controllers/snes.svg";
+        case "3ds":
+            return "/assets/img/controllers/nds.svg";
+        default:
+            return "/assets/img/bannerBlank.svg";
+    }
+}
+
+export const consoleLookup = {
+    "gb": "Nintendo GameBoy (NGB)",
+    "gbc": "Nintendo GameBoy Colour (GBC)",
+    "gba": "Nintendo GameBoy Advance (GBA)",
+    "n64": "Nintendo 64 (N64)",
+    "nds": "Nintendo DS / DSi (NDS)",
+    "nes": "Nintendo Entertainment System (NES)",
+    "gg": "SEGA Game Gear",
+    "segaMd": "SEGA MegaDrive / SEGA Genesis",
+    "sms": "SEGA Master System",
+    "snes": "Super Nintendo Entertainment System (SNES)",
+    "3ds": "Nintendo 3DS (3DS)"
+}
